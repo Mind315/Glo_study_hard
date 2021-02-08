e688c254c95981f018db7c91d5cf367aa8301b46
Mind315 <70093339+Mind315@users.noreply.github.com>
2021-02-08 21:16:06 +0300

    Исправил

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 'use strict';
 
-const week = [ 'понед', 'вторник', 'среда', 'четверг', 'пятница', 'сб','вс'];
+const week = ['вс', 'понед', 'вторник', 'среда', 'четверг', 'пятница', 'сб'];
 let date = new Date().getDay() ;
 let arr = [];
 
 week.forEach((el, i) => {
   arr.push(el);
-  if (el === 'сб' || el === 'вс') {
+  if (date === i) {
     arr[i] = el.italics();
-  }
-  if (date === (i - 6) && el === 'вс') {
+  } 
+  if (el === 'вс' || el === 'сб') {
+    arr[i] = el.bold();
+  }   
+  if ((el === 'вс' && date === i) || (el === 'сб' && date === i)) {
     arr[i] = el.italics().bold();
-  }
+  } 
 });
 
 
